2c47ed71da053a62e45a81804236c32fb67700d1
Joan Orellana <joan.orellanairios@studenti.unipd.it>
2023-05-06 13:17:02 +0200

    df audio features

--- test.R
+++ test.R
@@ -1,25 +1,23 @@
 setwd("/Users/joanorellanarios/Library/CloudStorage/GoogleDrive-joanorellanarios@gmail.com/La meva unitat/Master/Second Semester/Statistical Learning Mod B/SL_Project")
 
-install.packages("rjson")
-library("rjson")
-
 sp_y <- read.csv("Spotify_Youtube.csv", sep=',')
-grammy <- fromJSON(file = "grammy.json", flatten=TRUE)
+sp_y$unique <- paste(sp_y$Artist, "_", sp_y$Track)
+grammy <- read.csv("grammySongs.csv", sep=',')
+grammy$unique <- paste(grammy$Artist, "_", grammy$Name)
 
-json_data_frame <- as.data.frame(grammy)
+library(dplyr)
 
-attach(sp_y)
+df <- left_join(sp_y, grammy, by = "unique")
 
-hist(Views)
+summary(df)
 
-summary(sp_y)
+colnames(df)
 
+df = df[c('X.x', 'Artist.x', 'Track', 'Album', 'Album_type', 'Danceability', "Energy", "Key", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Valence", "Tempo", "Duration_ms", "Views", "Likes", "Comments", "GrammyAward", "GrammyYear", "Genre")]
 
+df$winner <- ifelse(!is.na(df$GrammyAward),TRUE, FALSE)
 
+audio_f <- df[c('Danceability', "Energy", "Key", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Valence", "Tempo")]
 
-
-
-
-
-
+cor(audio_f)
 
