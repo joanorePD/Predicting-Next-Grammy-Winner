7107f92c099560fbf65c2c2d1959d358362ee979
Joan Orellana <joan.orellanairios@studenti.unipd.it>
2023-05-06 12:30:54 +0200

    changed readme and added grammy dataset

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SL_Project
+
+Found grammy award dataset from:
+
+https://github.com/JLUT/Billboard_Grammy_Project1
+
+file named: grammySongs_1999-2019.csv
+
+Found Spotify and youtube dataset from:
+
+https://www.kaggle.com/datasets/salvatorerastelli/spotify-and-youtube
--- test.R
+++ test.R
@@ -1,13 +1,18 @@
 setwd("/Users/joanorellanarios/Library/CloudStorage/GoogleDrive-joanorellanarios@gmail.com/La meva unitat/Master/Second Semester/Statistical Learning Mod B/SL_Project")
+
+install.packages("rjson")
+library("rjson")
+
 sp_y <- read.csv("Spotify_Youtube.csv", sep=',')
+grammy <- fromJSON(file = "grammy.json", flatten=TRUE)
+
+json_data_frame <- as.data.frame(grammy)
 
 attach(sp_y)
 
 hist(Views)
 
-
-
-
+summary(sp_y)
 
 
 
