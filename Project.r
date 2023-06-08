da7b7e0f6643f141f9229e200fef7d674cb57fc3
Cristian Granchelli <cristian.granchelli@studenti.unipd.it>
2023-06-08 16:50:31 +0200

    More EDA

--- Project.r
+++ Project.r
@@ -102,15 +102,28 @@ sum(training_set$IsWinner == 1)/ sum(training_set$IsWinner == 0)
 
 # Exploratory Data Analysis
 
+# Relationship between independent variables 
+
+#Correlations 
+
 cor_matrix = cor(training_set[,c(-1, -2, -10, -13, -15)])
 corrplot(cor_matrix)
 pairs(training_set[,c(-1, -2, -10, -13, -15)], lower.panel = panel.smooth)
 
-summary(training_set)
+summary(data)
 
+install.packages("confintr")
+library(confintr)
+
+as.numeric(key)
+
+cramersv(matrix(c(as.numeric(key), as.numeric(mode)), ncol = 2))
+cramersv(matrix(c(as.numeric(key), as.numeric(time_signature)), ncol = 2))
+cramersv(matrix(c(as.numeric(mode), as.numeric(time_signature)), ncol = 2))
+
+
+# Partial correlations
 correlation(training_set[,c(-1, -2, -10, -13, -15)], partial = TRUE)
-
-
 
 ggplot(data = training_set, aes(danceability, valence)) + geom_jitter(color = "blue")
 ggplot(data = training_set, aes(loudness, energy)) + geom_jitter(color = "blue")
@@ -125,6 +138,8 @@ data[504, ]
 
 attach(training_set)
 
+#Checking distributions
+
 par(mfrow= c(2, 5))
 
 hist(followers)
@@ -138,9 +153,25 @@ hist(loudness)
 hist(tempo)
 hist(valence)
 
+
+#Relationship between dependent and independent variables
+
+boxplot(danceability ~ IsWinner)
+boxplot(followers ~ IsWinner)
+boxplot(acousticness ~ IsWinner)
+boxplot(duration_ms ~ IsWinner)
+boxplot(energy ~ IsWinner)
+boxplot(instrumentalness ~ IsWinner)
+boxplot(liveness ~ IsWinner)
+boxplot(loudness ~ IsWinner)
+boxplot(tempo ~ IsWinner)
+boxplot(valence ~ IsWinner)
+
 par(mfrow = c(1, 1))
 
 
+
+###############################################################################
 
 
 
